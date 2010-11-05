717c225fc4d85605b8226780bef60cabf12a26ce
Simon Michael <simon@joyful.com>
2010-11-04 20:03:18 -0700

    make commits tests pass

--- lib/scm/adapters/darcs/commits.rb
+++ lib/scm/adapters/darcs/commits.rb
@@ -2,16 +2,15 @@ module Scm::Adapters
 	class DarcsAdapter < AbstractAdapter
 
 		# Return the number of commits in the repository following +since+.
-		def commit_count(since=0)
-			commit_tokens(since || 0).size
+		def commit_count(since=nil)
+			commit_tokens(since).size
 		end
 
 		# Return the list of commit tokens following +since+.
-		def commit_tokens(since=0, up_to='tip')
-			# We reverse the final result in Ruby, rather than passing the --reverse flag to darcs.
-			# That's because the -f (follow) flag doesn't behave the same in both directions.
-			# Basically, we're trying very hard to make this act just like Git. The darcs_rev_list_test checks this.
-			tokens = run("cd '#{self.url}' && darcs log -f -r #{up_to || 'tip'}:#{since || 0} --template='{node}\\n'").split("\n").reverse
+		def commit_tokens(since=nil, up_to=nil)
+      from = since ? " --from-patch #{since}" : ""
+      to   = up_to ? " --to-patch #{up_to}" : ""
+			tokens = string_to_patch_names(run("cd '#{self.url}' && darcs changes#{from}#{to}")).reverse
 
 			# Darcs returns everything after *and including* since.
 			# We want to exclude it.
@@ -26,10 +25,10 @@ module Scm::Adapters
 		# Not including the diffs is meant to be a memory savings when we encounter massive repositories.
 		# If you need all commits including diffs, you should use the each_commit() iterator, which only holds one commit
 		# in memory at a time.
-		def commits(since=0)
-			count = run("cd '#{self.url}' && darcs changes --count")
-			log = run("cd '#{self.url}' && darcs changes --last #{count-since}")
-			a = Scm::Parsers::DarcsParser.parse(log).reverse
+		def commits(since=nil)
+			from = since ? " --from-patch #{since}" : ""
+			log = run("cd '#{self.url}' && darcs changes#{from} --reverse")
+			a = Scm::Parsers::DarcsParser.parse(log)
 			if a.any? && a.first.token == since
 				a[1..-1]
 			else
@@ -47,7 +46,7 @@ module Scm::Adapters
 		# The log is stored in a temporary file.
 		# This is designed to prevent excessive RAM usage when we encounter a massive repository.
 		# Only a single commit is ever held in memory at once.
-		def each_commit(since=0)
+		def each_commit(since=nil)
 			open_log_file(since) do |io|
 				Scm::Parsers::DarcsParser.parse(io) do |commit|
 					yield commit if block_given? && commit.token != since
@@ -56,21 +55,23 @@ module Scm::Adapters
 		end
 
 		# Not used by Ohloh proper, but handy for debugging and testing
-		def log(since=0)
-			run "cd '#{url}' && darcs log -f -v -r tip:#{since}"
+		def log(since=nil)
+      from = since ? " --from-patch #{since}" : ""
+			run "cd '#{url}' && darcs changes -s#{from}"
 		end
 
 		# Returns a file handle to the log.
 		# In our standard, the log should include everything AFTER +since+. However, darcs doesn't work that way;
 		# it returns everything after and INCLUDING +since+. Therefore, consumers of this file should check for
 		# and reject the duplicate commit.
-		def open_log_file(since=0)
+		def open_log_file(since=nil)
 			begin
 				if since == head_token # There are no new commits
 					# As a time optimization, just create an empty file rather than fetch a log we know will be empty.
 					File.open(log_filename, 'w') { }
 				else
-					run "cd '#{url}' && darcs log --verbose -r #{since || 0}:tip --style #{Scm::Parsers::DarcsParser.verbose_style_path} > #{log_filename}"
+          from = since ? " --from-patch #{since}" : ""
+					run "cd '#{url}' && darcs changes --reverse -v#{from} > #{log_filename}"
 				end
 				File.open(log_filename, 'r') { |io| yield io }
 			ensure
--- test/unit/darcs_commits_test.rb
+++ test/unit/darcs_commits_test.rb
@@ -5,32 +5,18 @@ module Scm::Adapters
 
 		def test_commit
 			with_darcs_repository('darcs') do |darcs|
-				assert_equal 4, darcs.commit_count
-				assert_equal 2, darcs.commit_count('b14fa4692f949940bd1e28da6fb4617de2615484')
-				assert_equal 0, darcs.commit_count('75532c1e1f1de55c2271f6fd29d98efbe35397c4')
-
-				assert_equal ['01101d8ef3cea7da9ac6e9a226d645f4418f05c9',
-											'b14fa4692f949940bd1e28da6fb4617de2615484',
-											'468336c6671cbc58237a259d1b7326866afc2817',
-											'75532c1e1f1de55c2271f6fd29d98efbe35397c4'], darcs.commit_tokens
-
-				assert_equal ['75532c1e1f1de55c2271f6fd29d98efbe35397c4'],
-					darcs.commit_tokens('468336c6671cbc58237a259d1b7326866afc2817')
-
-				assert_equal [], darcs.commit_tokens('75532c1e1f1de55c2271f6fd29d98efbe35397c4')
-
-				assert_equal ['01101d8ef3cea7da9ac6e9a226d645f4418f05c9',
-											'b14fa4692f949940bd1e28da6fb4617de2615484',
-											'468336c6671cbc58237a259d1b7326866afc2817',
-											'75532c1e1f1de55c2271f6fd29d98efbe35397c4'], darcs.commits.collect { |c| c.token }
-
-				assert_equal ['75532c1e1f1de55c2271f6fd29d98efbe35397c4'],
-					darcs.commits('468336c6671cbc58237a259d1b7326866afc2817').collect { |c| c.token }
-
+				assert_equal 2, darcs.commit_count
+				assert_equal 1, darcs.commit_count('add helloworld.c')
+				assert_equal 0, darcs.commit_count('remove helloworld.c')
+				assert_equal ['add helloworld.c', 'remove helloworld.c'], darcs.commit_tokens
+				assert_equal ['remove helloworld.c'], darcs.commit_tokens('add helloworld.c')
+				assert_equal [], darcs.commit_tokens('remove helloworld.c')
+				assert_equal ['add helloworld.c',
+											'remove helloworld.c'], darcs.commits.collect { |c| c.token }
+				assert_equal ['remove helloworld.c'], darcs.commits('add helloworld.c').collect { |c| c.token }
 				# Check that the diffs are not populated
-				assert_equal [], darcs.commits('468336c6671cbc58237a259d1b7326866afc2817').first.diffs
-
-				assert_equal [], darcs.commits('75532c1e1f1de55c2271f6fd29d98efbe35397c4')
+				assert_equal [], darcs.commits('add helloworld.c').first.diffs
+				assert_equal [], darcs.commits('remove helloworld.c')
 			end
 		end
 
@@ -38,10 +24,8 @@ module Scm::Adapters
 			commits = []
 			with_darcs_repository('darcs') do |darcs|
 				darcs.each_commit do |c|
-					assert c.token.length == 40
-					assert c.committer_name
-					assert c.committer_date.is_a?(Time)
-					assert c.message.length > 0
+					assert c.author_name
+					assert c.author_date.is_a?(Time)
 					assert c.diffs.any?
 					# Check that the diffs are populated
 					c.diffs.each do |d|
@@ -53,10 +37,8 @@ module Scm::Adapters
 				assert !FileTest.exist?(darcs.log_filename) # Make sure we cleaned up after ourselves
 
 				# Verify that we got the commits in forward chronological order
-				assert_equal ['01101d8ef3cea7da9ac6e9a226d645f4418f05c9',
-											'b14fa4692f949940bd1e28da6fb4617de2615484',
-											'468336c6671cbc58237a259d1b7326866afc2817',
-											'75532c1e1f1de55c2271f6fd29d98efbe35397c4'], commits.collect { |c| c.token }
+				assert_equal ['add helloworld.c',
+											'remove helloworld.c'], commits.map {|c| c.token}
 			end
 		end
 	end
